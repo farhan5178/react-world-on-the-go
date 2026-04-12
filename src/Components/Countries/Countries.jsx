edd90cff6dd7a4785b2bf6493aa2dfe72dbef7aa
Farhan Sadik Turjo <farhansaditurjo1265@gmail.com>
2026-04-12 16:00:58 +0600

    first commit

---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,23 @@
+// function Countries (){
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+// // 
+// const Countries =()=>{
+//     return()
+// }
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1> in the countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
